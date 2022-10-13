a9e8bb32fa85488d5557128dd384c3dc64b82fc4
JUNCHENG LIU <1481610321@qq.com>
2022-10-13 15:45:07 +0800

    The main and matrix.h
    It is the skeleton of the main and matrix.h

---
+++ main.c
@@ -0,0 +1,37 @@
+//
+//  main.c
+//  DKU Semester I
+//
+//  Created by Jamie on 2022/10/13.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix.h"
+
+
+int main(int argc, char ** argv){
+    // regular check
+    if(argc != 3){
+        perror("Argument Number Error!");
+        return EXIT_FAILURE;
+    }
+    
+    // import and read the first and second file
+    FILE * firstFile = fopen("matrix1.txt", "r");
+    matrix_t * firstMatrix = malloc(sizeof(*firstMatrix));
+    firstMatrix = readMatrix(firstFile);
+    
+    FILE * secondFile = fopen("matrix2.txt", "r");
+    matrix_t * secondMatrix = malloc (sizeof(*firstMatrix));
+    secondMatrix = readMatrix(secondFile);
+    
+    matrix_t * result = malloc(sizeof(*result));
+    result = multiply(firstMatrix, secondMatrix);
+    
+    printMatrix(result);
+    
+    freeMatrix(firstMatrix);
+    freeMatrix(secondMatrix);
+    freeMatrix(result);
+}
---
+++ matrix.h
@@ -0,0 +1,17 @@
+//
+//  matrix.h
+//  DKU Semester I
+//
+//  Created by Jamie on 2022/10/13.
+//
+
+#ifndef matrix_h
+#define matrix_h
+
+typedef struct{
+    double ** values;
+    size_t rows;
+    size_t columns;
+}matrix_t;
+
+#endif /* matrix_h */
