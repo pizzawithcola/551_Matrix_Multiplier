88e08cd589eba330009286ed068fb6ac9f6b55a4
ruolin.meng@uconn.edu <ruolin.meng@uconn.edu>
2022-10-14 02:37:45 +0800

    finished

--- freeAndPrint.c
+++ freeAndPrint.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include "matrix.h"
 
-
+//this function free a matrix_t
 void freeMatrix(matrix_t * matrix) {
-  //WRITE ME!
   for(int i = 0; i < matrix->rows; i++){
     free(matrix->values[i]);
   }
   free(matrix->values);
   free(matrix);
 }
-//
+
+//this function prints matrix_t
 void printMatrix(matrix_t * matrix){
     for(size_t i = 0; i < matrix->rows; i++){
         for(size_t j = 0; j < matrix->columns; j++){
